22d5753e2c57e06a6835864b58d55e0c7f5c3fa1
Danush Ramanan <dan.ramanan@gmail.com>
2025-10-19 11:34:15 -0600

    checkerboard

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,32 @@
 #include "raylib.h"
+#include <iostream>
 
 int main()
 {
-    InitWindow(800, 800, "Duckfish");
+    const Color CHECKERBOARD_LIGHT = (Color){240, 216, 181, 255};
+    const Color CHECKERBOARD_DARK = (Color){180, 136, 98, 255};
+
+    const int SQUARE_SIZE = 80;
+
+    InitWindow(SQUARE_SIZE * 8, SQUARE_SIZE * 8, "Duckfish");
 	SetWindowState(FLAG_VSYNC_HINT);
 	SetTargetFPS(60);
 
         while (!WindowShouldClose())
         {
-		// LOGIC
+    		BeginDrawing();
 
-		// DRAWING
+            ClearBackground(BLACK);
 
-		BeginDrawing();
+            for (int row = 0; row < 8; row++) {
+                for (int col = 0; col < 8; col++) {
+                    Color c = ((row + col) % 2 == 0) ? CHECKERBOARD_LIGHT : CHECKERBOARD_DARK;
+                    DrawRectangle(col * SQUARE_SIZE, (7 - row) * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE, c);
+                }
+            }
 
-        ClearBackground(BLACK);
-
-		EndDrawing();
-	}
+    		EndDrawing();
+    	}
 
 	CloseWindow();
 	return 0;
